831e097ca3feccf3afd4d1183638f0e0d0741157
Adiara sall <adiarasall208@gmail.com>
2024-11-08 20:30:13 +0000

    ajoute le fichier

---
+++ Tribulle.java
@@ -0,0 +1,23 @@
+public  class Tribulle{
+
+public static void main(String[]adiara){
+int tab []={14,7,10,17,9,5,4,13,2,11,6};
+int i,k,tmp;
+
+
+	for (int k=i+1;k<tab.length ;k++){
+	
+		if([tab[i]<tab[i-1]) {
+		tmp=tab[i];
+		tab[i]=tab[i-1];
+		tab[i-1]=tmp;
+
+}
+
+
+System.out.println(tab[k]);                    
+}
+}
+}
+
+
